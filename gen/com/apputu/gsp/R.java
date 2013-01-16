7359191d7e0ce2509d2e9da0ba2cd282f425990e
AndyYou <andyyu0920@gmail.com>
2013-01-16 21:09:58 +0800

    Add image show coordinate

--- gen/com/apputu/gsp/R.java
+++ gen/com/apputu/gsp/R.java
@@ -11,11 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int screen=0x7f020001;
+        public static final int circle=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int screen=0x7f020002;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int main=0x7f070000;
+        public static final int menu_settings=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/apputu/gsp/MainActivity.java
+++ src/com/apputu/gsp/MainActivity.java
@@ -2,9 +2,15 @@ package com.apputu.gsp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Point;
 import android.view.Menu;
 import android.view.MotionEvent;
+import android.widget.AbsoluteLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
 
@@ -26,6 +32,16 @@ public class MainActivity extends Activity {
 		if(event.getAction() == MotionEvent.ACTION_DOWN) {
 			String text = "X: " + event.getX() + " Y: " + event.getY();
 			Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+			int x = (int)event.getX();
+			int y = (int)event.getY();
+			RelativeLayout rlMain = (RelativeLayout) findViewById(R.id.main);
+			rlMain.removeAllViews();
+			ImageView iv = new ImageView(this);
+			iv.setImageResource(R.drawable.circle);
+			RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(40, 40);
+			params.topMargin=y-40;
+			params.leftMargin=x;
+			rlMain.addView(iv, params);
 		}
 
 		return super.onTouchEvent(event);
